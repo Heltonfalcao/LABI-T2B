5100aadadace5d95cbe1969b244048c28e488068
helton falcao <helton.falcao@hotmail.com>
2017-11-27 20:01:14 -0300

    inicio projeto

---
+++ WebAppHFA/Models/Musica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppHFA.Models
+{
+    public class Musica
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        public Categoria Categoria { get; set; }
+        public TipoMidia TipoMidia { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+
+
+    }
+}
